0ca443dbde37dd034531338f676c1e243ee85743
Kevin Yarmosh <yarmoskw@gmail.com>
2016-06-20 17:53:44 -0400

    Initial commit?

---
+++ Assets/Scripts/PlayerSphere.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSphere : VRTK_InteractableObject
+{
+    [Header("Game Objects", order = 4)]
+    public GameObject player;
+    public int touchForceMultiplier = 100;
+    public ushort hapticFeedbackStrength = 500;
+
+    VRTK_ControllerActions controllerActions;
+
+    public override void StopTouching(GameObject touchingObject)
+    {
+        base.StopTouching(touchingObject);
+        controllerActions = touchingObject.GetComponent<VRTK_ControllerActions>();
+        Rigidbody prb = player.GetComponent<Rigidbody>();
+        Vector3 impact = touchingObject.transform.localPosition.normalized * touchForceMultiplier;
+        prb.AddForce(impact);
+        controllerActions.TriggerHapticPulse(10, hapticFeedbackStrength);
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        if (player.transform.position.y < -100)
+        {
+            player.transform.position = new Vector3 (0, 1, 0);
+            Rigidbody prb = player.GetComponent<Rigidbody>();
+            prb.velocity = Vector3.zero;
+            prb.angularVelocity = Vector3.zero;
+        }
+    }
+}
---
+++ Assets/Shaders/CullOffShader.shader
@@ -0,0 +1,38 @@
+﻿Shader "Custom/CullOffShader" {
+	Properties{
+		_MainTex("Color (RGB) Alpha (A)", 2D) = "white"
+	}
+		SubShader{
+
+		Tags{ "Queue" = "Transparent" "RenderType" = "Transparent" }
+
+		Cull Front
+
+		CGPROGRAM
+
+#pragma surface surf Lambert vertex:vert alpha
+		sampler2D _MainTex;
+
+	struct Input {
+		float2 uv_MainTex;
+		float4 color : COLOR;
+	};
+
+
+	void vert(inout appdata_full v)
+	{
+		v.normal.xyz = v.normal * -1;
+	}
+
+	void surf(Input IN, inout SurfaceOutput o) {
+		fixed4 result = tex2D(_MainTex, IN.uv_MainTex);
+		o.Albedo = result.rgb * result.a;
+		o.Alpha = result.a;
+	}
+
+	ENDCG
+
+	}
+
+		Fallback "Diffuse"
+}
