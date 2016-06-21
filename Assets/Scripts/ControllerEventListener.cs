3494e7aaa7a3eda4fbbff5dbf6f1a335d1710582
Kevin Yarmosh <yarmoskw@gmail.com>
2016-06-21 13:23:22 -0400

    adds big jump. adds braking. braking not yet tied to trigger strength.

---
+++ Assets/Scripts/ControllerEventListener.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControllerEventListener : MonoBehaviour
+{
+    public PlayerSphere playerSphere;
+    public GameState gameState;
+    // Use this for initialization
+    void Start()
+    {
+        if (GetComponent<VRTK_ControllerEvents>() == null)
+        {
+            Debug.LogError("ControllerEventListener is required to be attached to a SteamVR Controller that has the VRTK_ControllerEvents script attached to it");
+            return;
+        }
+
+        //Setup controller event listeners
+        GetComponent<VRTK_ControllerEvents>().TriggerClicked += new ControllerClickedEventHandler(DoTriggerClicked);
+        GetComponent<VRTK_ControllerEvents>().TriggerUnclicked += new ControllerClickedEventHandler(DoTriggerUnclicked);
+
+        GetComponent<VRTK_ControllerEvents>().TriggerAxisChanged += new ControllerClickedEventHandler(DoTriggerAxisChanged);
+
+        GetComponent<VRTK_ControllerEvents>().ApplicationMenuClicked += new ControllerClickedEventHandler(DoApplicationMenuClicked);
+        GetComponent<VRTK_ControllerEvents>().ApplicationMenuUnclicked += new ControllerClickedEventHandler(DoApplicationMenuUnclicked);
+
+        GetComponent<VRTK_ControllerEvents>().GripClicked += new ControllerClickedEventHandler(DoGripClicked);
+        GetComponent<VRTK_ControllerEvents>().GripUnclicked += new ControllerClickedEventHandler(DoGripUnclicked);
+
+        GetComponent<VRTK_ControllerEvents>().TouchpadClicked += new ControllerClickedEventHandler(DoTouchpadClicked);
+        GetComponent<VRTK_ControllerEvents>().TouchpadUnclicked += new ControllerClickedEventHandler(DoTouchpadUnclicked);
+
+        GetComponent<VRTK_ControllerEvents>().TouchpadTouched += new ControllerClickedEventHandler(DoTouchpadTouched);
+        GetComponent<VRTK_ControllerEvents>().TouchpadUntouched += new ControllerClickedEventHandler(DoTouchpadUntouched);
+
+        GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged += new ControllerClickedEventHandler(DoTouchpadAxisChanged);
+    }
+
+    void DebugLogger(uint index, string button, string action, ControllerClickedEventArgs e)
+    {
+        Debug.Log("Controller on index '" + index + "' " + button + " has been " + action
+                + " with a pressure of " + e.buttonPressure + " / trackpad axis at: " + e.touchpadAxis + " (" + e.touchpadAngle + " degrees)");
+    }
+
+    void DoTriggerClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TRIGGER", "pressed down", e);
+        playerSphere.AddBraker(sender);
+    }
+
+    void DoTriggerUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TRIGGER", "released", e);
+        playerSphere.RemoveBraker(sender);
+    }
+
+    void DoTriggerAxisChanged(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TRIGGER", "axis changed", e);
+    }
+
+    void DoApplicationMenuClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "APPLICATION MENU", "pressed down", e);
+        playerSphere.ResetPlayer();
+    }
+
+    void DoApplicationMenuUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "APPLICATION MENU", "released", e);
+    }
+
+    void DoGripClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "GRIP", "pressed down", e);
+    }
+
+    void DoGripUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "GRIP", "released", e);
+    }
+
+    void DoTouchpadClicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "pressed down", e);
+    }
+
+    void DoTouchpadUnclicked(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "released", e);
+    }
+
+    void DoTouchpadTouched(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "touched", e);
+    }
+
+    void DoTouchpadUntouched(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "untouched", e);
+    }
+
+    void DoTouchpadAxisChanged(object sender, ControllerClickedEventArgs e)
+    {
+        DebugLogger(e.controllerIndex, "TOUCHPAD", "axis changed", e);
+    }
+}
--- Assets/Scripts/PlayerSphere.cs
+++ Assets/Scripts/PlayerSphere.cs
@@ -9,7 +9,11 @@ public class PlayerSphere : VRTK_InteractableObject
     public int touchForceMultiplier = 100;
     public ushort hapticFeedbackStrength = 500;
 
+    ArrayList brakingControllers = new ArrayList(2); 
     VRTK_ControllerActions controllerActions;
+    private float strength = 0;
+    private float smoothTime = 40.0f;
+    private bool reset = false;
 
     public override void StartTouching(GameObject touchingObject)
     {
@@ -32,13 +36,57 @@ public class PlayerSphere : VRTK_InteractableObject
     protected override void Update()
     {
         base.Update();
-        if (player.transform.position.y < -100)
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+        if (reset)
         {
-            player.transform.position = new Vector3 (0, 1, 0);
-            Rigidbody prb = player.GetComponent<Rigidbody>();
-            prb.velocity = Vector3.zero;
-            prb.angularVelocity = Vector3.zero;
-            gameState.SetUnready();
+            DoResetPlayer();
         }
+        if (brakingControllers.Count > 0)
+        {
+            DoStop();
+        }
+    }
+
+    private void DoStop()
+    {
+        Rigidbody prb = player.GetComponent<Rigidbody>();
+        Mathf.SmoothDamp(0.0f, 1.0f, ref strength, smoothTime);
+        Vector3 f = -(prb.mass * prb.velocity) * strength;
+        Vector3 t= -(prb.mass * prb.angularVelocity) * strength;
+
+        prb.AddForce(f, ForceMode.Impulse);
+        prb.AddTorque(t, ForceMode.Impulse);
+        //prb.AddForce(-(Physics.gravity) * strength, ForceMode.Acceleration);
+        //prb.AddTorque(-(Physics.gravity) * strength, ForceMode.Acceleration);
+    }
+
+    public void AddBraker(object sender)
+    {
+        brakingControllers.Add(sender);
+    }
+
+    public void RemoveBraker(object sender)
+    {
+        brakingControllers.Remove(sender);
+    }
+
+    public void ResetPlayer()
+    {
+        reset = true;
+    }
+
+    private void DoResetPlayer()
+    {
+        reset = false;
+        player.transform.position = new Vector3(0, 1, 0);
+        //player.transform.position = new Vector3(-30.78f, -72.091f, 303.43f);
+        Rigidbody prb = player.GetComponent<Rigidbody>();
+        prb.velocity = Vector3.zero;
+        prb.angularVelocity = Vector3.zero;
+        gameState.SetUnready();
     }
 }
