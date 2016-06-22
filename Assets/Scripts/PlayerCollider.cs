b30d13bae4932eedcb5470417c553d177fea1cb0
Kevin Yarmosh <yarmoskw@gmail.com>
2016-06-22 16:26:51 -0400

    Reset player after touching terrain.

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerCollider : MonoBehaviour {
+    public PlayerSphere playerSphere;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Terrain")
+        {
+            playerSphere.ResetPlayer();
+        }
+    }
+}
