04d4b816c387680b4aa55876b62c8a97c12b3f5f
Kevin Yarmosh <yarmoskw@gmail.com>
2017-01-13 17:09:12 -0500

    adds torus inside sphere so you can know when you are in sphere better.

---
+++ Assets/Scripts/PlayerTorus.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRTK;
+
+namespace Game
+{
+
+    public class PlayerTorus : MonoBehaviour
+    {
+        [Header("Game Objects", order = 4)]
+        public GameObject player;
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            transform.position = new Vector3(player.transform.position.x + 1, player.transform.position.y, player.transform.position.z);
+            transform.rotation = Quaternion.identity; 
+        }
+    }
+}
