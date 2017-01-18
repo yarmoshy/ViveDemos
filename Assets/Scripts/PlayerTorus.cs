afb5f1f5d3522213f5b7384122db2210b705ebbc
Kevin Yarmosh <yarmoskw@gmail.com>
2017-01-17 20:58:47 -0500

    working on lvl 2 warp

--- Assets/Scripts/PlayerTorus.cs
+++ Assets/Scripts/PlayerTorus.cs
@@ -9,12 +9,28 @@ namespace Game
     {
         [Header("Game Objects", order = 4)]
         public GameObject player;
+        public PlayerSphere playerSphere;
+
+        private Quaternion startRotation;
+        private Quaternion startLocalRotation;
+
 
         // Update is called once per frame
+        void Start()
+        {
+            startRotation = transform.rotation;
+            startLocalRotation = transform.localRotation;
+        }
+
         void LateUpdate()
         {
-            transform.position = new Vector3(player.transform.position.x + 1f, player.transform.position.y, player.transform.position.z);
-            transform.rotation = Quaternion.identity;
+            //transform.rotation = playerSphere.transform.rotation;
+            //transform.position = new Vector3(player.transform.position.x + 1f, player.transform.position.y, player.transform.position.z);
+
+            // transform.localRotation = player.transform.localRotation;
+            //  transform.position = new Vector3(player.transform.position.x + 1f, player.transform.position.y, player.transform.position.z);
+            transform.rotation = startRotation;
+                transform.localRotation = startLocalRotation;
         }
     }
 }
